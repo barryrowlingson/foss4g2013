8393de10d0c57913e87224da3b1bf48432944427
Barry Rowlingson <b.rowlingson@gmail.com>
2012-11-15 22:18:58 +0000

    new front page, functions

---
+++ functions.php
@@ -0,0 +1,22 @@
+function image_shortcode($atts, $content = null) {
+    extract( shortcode_atts( array(
+    'name' => '',
+    'align' => 'right',
+    'ext' => 'png',
+    'path' => '/wp-content/uploads/',
+    'url' => ''
+    ), $atts ) );
+    $file=ABSPATH."$path$name.$ext";
+    if (file_exists($file)) {
+        $size=getimagesize($file);
+        if ($size!==false) $size=$size[3];
+        $output = "<img src='".get_option('siteurl')."$path$name.$ext' alt='$name' $size align='$align' class='align$align' />";
+        if ($url) $output = "<a href='$url' title='$name'>".$output.'</a>';
+        return $output;
+    }
+    else {
+        trigger_error("'$path$name.$ext' image not found", E_USER_WARNING);
+        return '';
+    }
+}
+add_shortcode('image','image_shortcode');
