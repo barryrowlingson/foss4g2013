8c41efe30a4eedf67c6b4d3b56dccf7d7bb68d85
Barry Rowlingson <B.Rowlingson@gmail.com>
2012-11-20 10:28:09 +0000

    get sponsor list fucntion

--- functions.php
+++ functions.php
@@ -21,4 +21,50 @@ function image_shortcode($atts, $content = null) {
     }
 }
 add_shortcode('image','image_shortcode');
+
+function sponsorlist_shortcode($atts,$content=null){
+  $tops = Foss4g::topsponsors();
+  
+  $output="<ul>";
+  
+  foreach (array("Diamond","Platinum","Gold") as $level){
+    $output .= "<li class=\"level ".$level."\">";
+    $output .= $level." Sponsors";
+    $output.="<ul class=\"sponsors\">";
+    foreach ($tops[$level] as $sponsor){
+      $output.="<li class=\"sponsor\">".$sponsor->post_title."</li>";
+    };
+    $output.="</ul>";
+
+    $output .="</li>";
+  };
+  
+
+  return $output;
+
+};
+add_shortcode('sponsorlist','sponsorlist_shortcode');
+
+new Foss4g();
+class Foss4g{
+
+  function topsponsors(){
+    $levels = Conferencer::get_posts('sponsor_level');
+    foreach ($levels as $id => $level) {
+      $levels[$id]->sponsors = array();
+    }
+
+    $tops=array("Diamond"=>array(),"Platinum"=>array(),"Gold"=>array());
+
+    foreach (Conferencer::get_posts('sponsor') as $sponsor) {
+      Conferencer::add_meta($sponsor);
+      if (array_key_exists($levels[$sponsor->level]->post_title,$tops)){
+	array_push($tops[$levels[$sponsor->level]->post_title],$sponsor);
+      }
+    }
+    return $tops;
+  }
+
+};
+
 ?>
