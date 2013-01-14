dd447af3a15d76c4304442f8323de749b1025b05
Barry Rowlingson <B.Rowlingson@gmail.com>
2013-01-14 09:53:37 +0000

    tweaking sponsor list page

--- archive-sponsor.php
+++ archive-sponsor.php
@@ -17,7 +17,11 @@ if ( !defined('ABSPATH')) exit;
  */
 ?>
 <?php get_header(); ?>
-<h1>Special sponsor archive</h1>
+<h1>Our Sponsors</h1>
+<h2>Todo</h2>
+Loop over levels, then loop over sponsors and display sponsor summary. Meanwhile, here's a slightly edited 
+loop over posts.
+
         <div id="contentbox" class="grid col-620">
         <div id="content">
 <?php if (have_posts()) : ?>
@@ -25,49 +29,21 @@ if ( !defined('ABSPATH')) exit;
 		<?php while (have_posts()) : the_post(); ?>
         
         <?php $options = get_option('responsive_theme_options'); ?>
-		<?php if ($options['breadcrumb'] == 0): ?>
-		<?php echo responsive_breadcrumb_lists(); ?>
-        <?php endif; ?> 
+		
           
             <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <h1 class="post-title"><?php the_title(); ?></h1>
 
-                <div class="post-meta">
-                <?php responsive_post_meta_data(); ?>
-                
-				    <?php if ( comments_open() ) : ?>
-                        <span class="comments-link">
-                        <span class="mdash">&mdash;</span>
-                    <?php comments_popup_link(__('No Comments &darr;', 'responsive'), __('1 Comment &darr;', 'responsive'), __('% Comments &darr;', 'responsive')); ?>
-                        </span>
-                    <?php endif; ?> 
-                </div><!-- end of .post-meta -->
+     
                                 
                 <div class="post-entry">
                     <?php the_content(__('Read more &#8250;', 'responsive')); ?>
                     
-                    <?php if ( get_the_author_meta('description') != '' ) : ?>
-                    
-                    <div id="author-meta">
-                    <?php if (function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email'), '80' ); }?>
-                        <div class="about-author"><?php _e('About','responsive'); ?> <?php the_author_posts_link(); ?></div>
-                        <p><?php the_author_meta('description') ?></p>
-                    </div><!-- end of #author-meta -->
-                    
-                    <?php endif; // no description, no author's meta ?>
-                    
-                    <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'responsive'), 'after' => '</div>')); ?>
                 </div><!-- end of .post-entry -->
                 
-                <div class="navigation">
-			        <div class="previous"><?php previous_post_link( '&#8249; %link' ); ?></div>
-                    <div class="next"><?php next_post_link( '%link &#8250;' ); ?></div>
-		        </div><!-- end of .navigation -->
+               
                 
-                <div class="post-data">
-				    <?php the_tags(__('Tagged with:', 'responsive') . ' ', ', ', '<br />'); ?> 
-					<?php printf(__('Posted in %s', 'responsive'), get_the_category_list(', ')); ?> 
-                </div><!-- end of .post-data -->             
+            
 
             <div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div>             
             </div><!-- end of #post-<?php the_ID(); ?> -->
