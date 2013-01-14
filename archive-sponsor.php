52e35043f1e37156a868b49764d9d8685b758d6c
Barry Rowlingson <B.Rowlingson@gmail.com>
2013-01-14 16:14:41 +0000

    new style sponsors page

--- archive-sponsor.php
+++ archive-sponsor.php
@@ -17,59 +17,16 @@ if ( !defined('ABSPATH')) exit;
  */
 ?>
 <?php get_header(); ?>
+
+<div id="contentbox" class="grid col-620">
+<div id="content">
 <h1>Our Sponsors</h1>
-<h2>Todo</h2>
-Loop over levels, then loop over sponsors and display sponsor summary. Meanwhile, here's a slightly edited 
-loop over posts.
-
-        <div id="contentbox" class="grid col-620">
-        <div id="content">
-<?php if (have_posts()) : ?>
-
-		<?php while (have_posts()) : the_post(); ?>
-        
-        <?php $options = get_option('responsive_theme_options'); ?>
-		
-          
-            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <h1 class="post-title"><?php the_title(); ?></h1>
-
-     
-                                
-                <div class="post-entry">
-                    <?php the_content(__('Read more &#8250;', 'responsive')); ?>
-                    
-                </div><!-- end of .post-entry -->
-                
-               
-                
-            
-
-            <div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div>             
-            </div><!-- end of #post-<?php the_ID(); ?> -->
-            
-			<?php comments_template( '', true ); ?>
-            
-        <?php endwhile; ?> 
-
-        <?php if (  $wp_query->max_num_pages > 1 ) : ?>
-        <div class="navigation">
-			<div class="previous"><?php next_posts_link( __( '&#8249; Older posts', 'responsive' ) ); ?></div>
-            <div class="next"><?php previous_posts_link( __( 'Newer posts &#8250;', 'responsive' ) ); ?></div>
-		</div><!-- end of .navigation -->
-        <?php endif; ?>
-
-	    <?php else : ?>
-
-        <h1 class="title-404"><?php _e('404 &#8212; Fancy meeting you here!', 'responsive'); ?></h1>
-        <p><?php _e('Don&#39;t panic, we&#39;ll get through this together. Let&#39;s explore our options here.', 'responsive'); ?></p>
-        <h6><?php _e( 'You can return', 'responsive' ); ?> <a href="<?php echo home_url(); ?>/" title="<?php esc_attr_e( 'Home', 'responsive' ); ?>"><?php _e( '&larr; Home', 'responsive' ); ?></a> <?php _e( 'or search for the page you were looking for', 'responsive' ); ?></h6>
-        <?php get_search_form(); ?>
-
-<?php endif; ?>  
-      
-        </div><!-- end of #content -->
+<?php 
+echo sponsors_in(explode(",","Diamond,Platinum,Gold,Silver,Bronze,Supporter"),True);
+?>
 </div>
+</div>
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
